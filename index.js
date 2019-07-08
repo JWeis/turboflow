5850b020e6509d4400b7a476a0d0c26e2a1696d4
James Weis <jweis@revampagency.com>
2019-07-08 15:01:12 -0400

    refactor load script

--- index.js
+++ index.js
@@ -2,29 +2,32 @@ import Turbolinks from "turbolinks";
 
 Turbolinks.start();
 
+const body = document.querySelector("body");
+const js = body.querySelectorAll("script");
+const head = document.querySelector("head");
+
 document.addEventListener("turbolinks:load", function() {
   console.log("TurboFlow is Loaded!");
+  js.forEach(bs => {
+    body.removeChild(bs);
+    head.appendChild(bs);
+  });
 });
 
 // Remove & Replace Head
 document.addEventListener("turbolinks:before-cache", function() {
   console.log("before-cache");
   const script = document.querySelector("script.turboflow");
-  const head = document.querySelector("head");
   const title = document.querySelector("title");
   const meta = document.querySelectorAll("meta");
-  const body = document.querySelector("body");
-  const js = body.querySelectorAll("script");
-
   head.innerHTML = "";
   head.appendChild(title);
-  head.appendChild(link);
   head.appendChild(script);
   meta.forEach(m => {
     head.appendChild(m);
   });
 
-  js.forEach(script => {
-    body.removeChild(script);
+  js.forEach(s => {
+    body.removeChild(s);
   });
 });
